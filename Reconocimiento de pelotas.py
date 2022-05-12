9e5496ef1631eda084be02a5fc98f0352d8ac8ca
Miguel Sanchez Espinosa <102008847+MigSE@users.noreply.github.com>
2022-05-12 09:49:32 -0500

    Depuración
    Corrección en la publicación de coordenada
    Se publica la coordenada de acuerdo a la resolución de la pelota, al desaparecer de la vista se indica que no hay pelotas

--- Reconocimiento de pelotas.py
+++ Reconocimiento de pelotas.py
@@ -2,16 +2,13 @@ import cv2
 import numpy as np
 import urllib.request
 import requests
-#cap = cv2.VideoCapture(0)
-
-verdeBajo = np.array([40,45,107],np.uint8) 
-verdeAlto = np.array([60,255,255],np.uint8) 
+url='http://192.168.4.2/cam-hi.jpg' #url de la cámara ESP32 Cam
+resx=800 #resolución de cámara
+x=0
+verdeBajo = np.array([40,45,107],np.uint8)   #Frontera inferior de color verde 
+verdeAlto = np.array([60,255,255],np.uint8)  #Frontera superior de color verde
 while True:
-
-  #ret,frame = cap.read()
-
-  #if ret==True:
-  url='http://192.168.4.3/cam-hi.jpg'
+  
   img_resp=urllib.request.urlopen(url)
   imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
   frame = cv2.imdecode(imgnp,-1)
@@ -27,22 +24,24 @@ while True:
       if (M["m00"]==0): M["m00"]=1 
       x = int(M["m10"]/M["m00"])
       y = int(M['m01']/M['m00'])
-      if x > 450:
-        res=requests.post("http://192.168.4.1/right")
-      if x < 350:
-        r=requests.post("http://192.168.4.1/left")
-      if 350 < x <450:
-        r=requests.post("http://192.168.4.1/center")
-      #print(res.text)
       cv2.circle(frame, (x,y), 7, (0,255,0), -1)
       font = cv2.FONT_HERSHEY_SIMPLEX
       cv2.putText(frame, '{},{}'.format(x,y),(x+10,y), font, 0.75,(0,255,0),1,cv2.LINE_AA)
       nuevoContorno = cv2.convexHull(c)
       cv2.drawContours(frame, [nuevoContorno], 0, (255,0,0), 3)
     #cv2.imshow('maskAzul',mask)
-    else:
-      r=requests.post("http://192.168.4.1/null") 
-  cv2.imshow('frame',frame)
+  cv2.imshow('ESP32 Cam',frame)
+  if x > resx/2+10:
+    res=requests.post("http://192.168.4.1/right")
+  else: 
+    if 0 < x < resx/2-10:
+      r=requests.post("http://192.168.4.1/left")
+    else: 
+      if resx/2-10 < x < resx/2+10: 
+        r=requests.post("http://192.168.4.1/center")
+      else:
+        r=requests.post("http://192.168.4.1/null") 
+  x=0
   if cv2.waitKey(1) & 0xFF == ord('s'):
     break
 cap.release()
